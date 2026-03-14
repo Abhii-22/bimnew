91be4b2f4fa1ad4c6e70dcca8fda7c1c257ed802
Abhii-22 <137554703+Abhii-22@users.noreply.github.com>
2026-03-14 19:20:17 +0530

    coursedetails

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { FaClock, FaGraduationCap, FaUsers, FaBuilding, FaClipboardCheck, FaRocket, FaCheckCircle, FaAward, FaStar, FaLightbulb, FaTrophy } from 'react-icons/fa';
+import { FaClock, FaGraduationCap, FaUsers, FaClipboardCheck, FaRocket, FaCheckCircle, FaAward, FaStar, FaLightbulb, FaTrophy } from 'react-icons/fa';
 import './CourseDetail.css';
 
 
